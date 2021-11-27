b3478c1c34b076bdd1e213784a9d9b179f809c86
Levi Lunique <levi.lunique@gmail.com>
2021-11-27 18:58:15 -0500

    Initial Commit

---
+++ AgendaCFO/AgendaCFOApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgendaCFOApp.swift
+//  AgendaCFO
+//
+//  Created by user204006 on 11/27/21.
+//
+
+import SwiftUI
+
+@main
+struct AgendaCFOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
